f84a7fa59338b7ffd6ae30c91088ddc9869af975
Azize Aydoğdu <aydogduazize@gmail.com>
2021-02-08 01:24:18 +0300

    23.01.2021 Ders Kodları

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    public class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];//Dizinin eleman sayısı
+            for (int i = 0;  i< tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 2;
+            musteri1.MusteriNo = "123450";
+            musteri1.Adi = "Azize";
+            musteri1.Soyadi = "Aydoğdu";
+            musteri1.TcNo = "123456789";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "123456";
+            musteri2.SirketAdi = "Kodlamaio";
+            musteri2.VergiNo = "123456";
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+        }
+    }
+}
